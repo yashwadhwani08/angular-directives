826be1c07303112d669694c9fbec86317beb212e
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-29 10:51:35 +0530

    added functionality to the custom directive that confirms if the user wishes to navigate to an external link, host property used to listen to click event

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,11 +1,24 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-    selector: 'a[appSafeLink]',
-    standalone: true,
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
-    constructor() {
-        console.log('SafeLinkDirective is active!');
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      return;
     }
+
+    event.preventDefault();
+  }
 }
