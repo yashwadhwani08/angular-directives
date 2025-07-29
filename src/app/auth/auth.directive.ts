41eba4cc89b08d425979c1168e140d6866ab12b5
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-29 12:12:30 +0530

    using ng-template for conditional rendering, in directive using TemplateRef for conditional content and ViewContainerRef to take charge of the DOM where the directive is being used

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  inject,
+  input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -9,13 +16,17 @@ import { AuthService } from './auth.service';
 export class AuthDirective {
   userType = input.required<Permission>({ alias: 'appAuth' });
   private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef); // to hold the reference of the content which should be shown conditionally
+  private viewContainerRef = inject(ViewContainerRef); // takes charge of the DOM where the directive is being used
 
   constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-        console.log('SHOW ELEMENT');
+        // console.log('SHOW ELEMENT');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('DO NOT SHOW ELEMENT');
+        // console.log('DO NOT SHOW ELEMENT');
+        this.viewContainerRef.clear();
       }
     });
   }
