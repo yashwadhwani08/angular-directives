a83be2de6539c683a9fdc5a46cf606e1a2524866
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-29 11:03:37 +0530

    accepting query input parameter from the host element to which this directive is attached

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,6 +8,7 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  queryParam = input('myapp');
   constructor() {
     console.log('SafeLinkDirective is active!');
   }
@@ -17,7 +18,7 @@ export class SafeLinkDirective {
 
     if (wantsToLeave) {
       const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=myapp';
+      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
       return;
     }
 
